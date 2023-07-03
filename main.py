c45c28fef5482d16ba2cb294f36d20f7801fc265
Skrepysh <101453262+Skrepysh@users.noreply.github.com>
2023-07-04 01:38:33 +0300

    v2.7

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from pyselector import run
-program_version = "2.6"
+program_version = "2.7"
 run(program_version)
--- pyselector.py
+++ pyselector.py
@@ -1,16 +1,36 @@
 import configparser
 import logging
 import os
+import sys
 import shutil
 import time
+from progress.bar import Bar
 from pathlib import Path
 from tkinter import messagebox as msg
 from sys import exit
 
 
+def finish():
+    print("завершение работы...")
+    time.sleep(1)
+    sys.exit()
+
+
 def error():
     time.sleep(2)
     os.system("cls")
+
+
+userappdata = f'{os.environ["appdata"]}\\pySelector'
+if not os.path.exists(f"{userappdata}"):
+    os.mkdir(userappdata)
+    file = open(f"{userappdata}\\config.ini", "w+")
+    file.write('[paths]\n; write path to minecraft folder below (%appdata%/.minecraft if not filled)\nminecraft = \n'
+               '; do not use parameter below, if do not know, what are you doing\nprogram =')
+    file.close()
+    msg.showerror(title="Файлы были повреждены!",
+                  message="Файлы программы были повреждены! Восстановление завершено успешно, перезапуск программы...")
+    error()
 
 
 class ZeroSelector(Exception):
@@ -19,27 +39,25 @@ class ZeroSelector(Exception):
 
 def version_define(version):
     version = str(version)
-    unknown = "unknown"
     if version == "" or version == " ":
-        return unknown
+        return "DEV Build"
     else:
         return version
 
 
-def config(type):
+def config(type_of_work):
+    os.chdir(userappdata)
     settings = configparser.ConfigParser()
     settings.read("config.ini")
-    game_directory = settings["paths"]["minecraft"]
-    program_directory = settings["paths"]["program"]
-    if type == "mine":
+    game_directory = settings["paths"]["minecraft"].replace('"', '')
+    program_directory = settings["paths"]["program"].replace('"', '')
+    if type_of_work == "mine":
         if game_directory == "" or not os.path.exists(game_directory):
-            print(game_directory)
-            print(os.path.exists(game_directory))
             game_directory = "%appdata%/.minecraft"
             return game_directory
         else:
             return game_directory
-    if type == "prog":
+    if type_of_work == "prog":
         if program_directory == "" or not os.path.exists(program_directory):
             program_directory = os.getcwd()
             return program_directory
@@ -49,133 +67,100 @@ def config(type):
 
 programdir = config("prog")
 minedir = config("mine")
-os.chdir(f"{programdir}\mods")
-version_list = [e for e in os.listdir() if os.path.isdir(e)]
 
 
 def preparator(type_of_work):
     os.chdir(programdir)
-    if type_of_work == "error":
-        if not os.path.exists(f"{programdir}/backup"):
-            print("Нету папки backup:-(")
-            os.mkdir(f"{os.getcwd()}/backup")
-            time.sleep(2)
-            print("Папка создана, повторите попытку")
+    if not os.path.exists(f"{userappdata}/backup"):
+        os.mkdir(rf"{userappdata}\backup")
+        error()
+    try:
+        if not os.path.exists(f"{minedir}/mods"):
+            os.mkdir(f"{minedir}/mods")
             error()
-        try:
-            if not os.path.exists(f"{minedir}/mods"):
-                print("Нету папки mods в директории игры:-(")
-                os.chdir(minedir)
-                os.mkdir("mods")
-                print("Папка создана, повторите попытку")
-                error()
-        except FileNotFoundError:
-            os.system("cls")
-            msg.showerror(title="Ошибка",
-                          message="Не удалось создать папку mods в корне игры!\nПроверьте config.ini")
-            os.system(f"notepad {programdir}/config.ini")
+    except FileNotFoundError:
+        os.system("cls")
+        msg.showwarning(title="Ошибка",
+                        message="Не удалось создать папку mods в корне игры!\nПроверьте config.ini")
+        os.system(f"notepad {programdir}/config.ini")
+        return "stopnow"
+    if not os.path.exists(f"{userappdata}/modpacks"):
+        print("Отсутствует хранилище модпаков:-(")
+        pb1 = Bar("Создание", max=2)  # прогрессбар для создания папки с модпаками
+        os.mkdir(f"{userappdata}/modpacks")
+        pb1.next()
+        readme = open(f"{userappdata}/modpacks/readME PLS.txt", "w")
+        readme.write("put your modpacks in this folder")
+        readme.close()
+        pb1.next()
+        pb1.finish()
+        print("В корне программы создана папка modpacks, поместите туда свои модпаки")
+        error()
+    else:
+        if type_of_work == "init":
+            pass
+        if type_of_work == "error":
             return "stopnow"
-        if not os.path.exists(f"{programdir}/mods"):
-            print("Отсутствует хранилище модпаков:-(")
-            os.chdir(programdir)
-            os.mkdir(f"{os.getcwd()}/mods")
-            readme = open(f"{os.getcwd()}/mods/readME PLS.txt", "w")
-            readme.write("put your modpacks in this folder")
-            readme.close()
-            print("В корне программы создана папка mods, поместите туда свои модпаки")
-            time.sleep(2)
-            error()
-        else:
-            return "stopnow"
-    if type_of_work == "init":
-        if not os.path.exists(f"{programdir}/backup"):
-            print("Нету папки backup:-(")
-            os.mkdir(f"{os.getcwd()}/backup")
-            time.sleep(2)
-            print("Папка создана, повторите попытку")
-            error()
-        try:
-            if not os.path.exists(f"{minedir}/mods"):
-                print("Нету папки mods в директории игры:-(")
-                os.chdir(minedir)
-                os.mkdir("mods")
-                print("Папка создана, повторите попытку")
-                error()
-        except FileNotFoundError:
-            os.system("cls")
-            msg.showerror(title="Ошибка",
-                          message="Не удалось создать папку mods в корне игры!\nПроверьте config.ini")
-            os.system(f"notepad {programdir}/config.ini")
-            return "stopnow"
-        if not os.path.exists(f"{programdir}/mods"):
-            print("Отсутствует хранилище модпаков:-(")
-            os.chdir(programdir)
-            os.mkdir(f"{os.getcwd()}/mods")
-            readme = open(f"{os.getcwd()}/mods/readME PLS.txt", "w")
-            readme.write("put your modpacks in this folder")
-            readme.close()
-            print("В корне программы создана папка mods, поместите туда свои модпаки")
-            time.sleep(2)
-            error()
 
 
-def finish():
-    print("завершение работы...")
-    time.sleep(1)
-    exit()
-
-
-def verpicker(progver, vers_list):
+def worker(progver):
+    os.chdir(f"{userappdata}\\modpacks\\")
+    vers_list = [e for e in os.listdir() if os.path.isdir(e)]
+    os.chdir(minedir)
     print("Привет!")
     print("Версия программы: " + progver)
     print("Список модпаков:  ")
+    print("*")
     counter = 1
     divider = ". "
-    for ver in vers_list:
-        print(str(counter) + str(divider) + str(ver))
-        counter += 1
+    if len(vers_list) == 0:
+        print("Папка модпаков пуста\n*")
+        os.system(f"explorer {userappdata}\\modpacks\\")
+        finish()
+    else:
+        for ver in vers_list:
+            print(str(counter) + str(divider) + str(ver))
+            counter += 1
+    print("*")
     print("Чтобы восстановить бэкап напишите restore")
     selector = str(input("Выберите версию: "))
     if selector == "restore":
-        os.chdir(minedir)
-        shutil.rmtree(f"{minedir}/mods")
-        os.chdir(programdir)
-        shutil.copytree(f"{programdir}/backup", f"{minedir}/pySelector/")
-        os.chdir(minedir)
-        p = Path(f"{minedir}/pySelector/")
+        pb4 = Bar("Восстановление", max=3)
+        shutil.rmtree(f"{minedir}\\mods")
+        pb4.next()
+        shutil.copytree(f"{userappdata}\\backup", f"{minedir}\\tempfiles\\")
+        pb4.next()
+        p = Path(f"{minedir}\\tempfiles\\")
         p.rename("mods")
+        pb4.next()
+        pb4.finish()
         print("Бэкап восстановлен")
-        return "restored"
+        finish()
     elif selector == "q" or selector == "quit":
-        return "quit"
+        finish()
     else:
         selector = int(selector)
         if selector == 0:
             raise ZeroSelector
         else:
-            a = vers_list.pop(selector - 1)
-            print("Выбрана версия " + a)
-            return a
-
-
-def worker(version):
-    if version == "restored":
-        finish()
-    if version == "quit":
-        finish()
-    else:
-        print("работаю..")
-        os.chdir(minedir)
-        shutil.rmtree(f"{programdir}/backup")
-        shutil.copytree(f"{minedir}/mods", f"{programdir}/backup")
-        shutil.rmtree(f"{minedir}/mods")
-        os.chdir(f"{programdir}/mods/")
-        shutil.copytree(f"{programdir}/mods/{version}", f"{minedir}/pySelector")
-        os.chdir(minedir)
-        p = Path(f"{minedir}/pySelector/")
-        p.rename("mods")
-        print("готово")
-        finish()
+            user_choice = vers_list.pop(selector - 1)
+            print("Выбрана версия " + user_choice)
+            print("работаю..")
+            pb5 = Bar("Выполнение", max=5)
+            shutil.rmtree(f"{userappdata}\\backup")
+            pb5.next()
+            shutil.copytree(f"{minedir}\\mods", f"{userappdata}\\backup")
+            pb5.next()
+            shutil.rmtree(f"{minedir}\\mods")
+            pb5.next()
+            shutil.copytree(f"{userappdata}\\modpacks\\{user_choice}", f"{minedir}\\tempfiles\\")
+            pb5.next()
+            p = Path(f"{minedir}\\tempfiles\\")
+            p.rename("mods")
+            pb5.next()
+            pb5.finish()
+            print("готово")
+            finish()
 
 
 def run(version):
@@ -186,15 +171,18 @@ def run(version):
                 break
             else:
                 pass
-            os.chdir(f"{programdir}/mods")
-            worker(verpicker(progver, version_list))
-            break
+            if os.path.exists(f"{minedir}\\tempfiles\\"):
+                shutil.rmtree(f"{minedir}\\tempfiles")
+            else:
+                pass
+            os.chdir(f"{userappdata}/modpacks")
+            worker(progver)
         except IndexError:
             print("неверное значение\nперезапуск")
             error()
         except PermissionError:
-            msg.showerror(title="Ошибка доступа", message="Произошло повреждение файлов программы."
-                                                          "\nНеобходимо произвести запуск от имени администратора!")
+            msg.showerror(title="Ошибка доступа", message="Не удается получить доступ к какому-то файлу."
+                                                          "\nПопробуйте произвести запуск от имени администратора!")
             exit()
         except OverflowError:
             print("многацифер\nперезапуск")
@@ -203,10 +191,8 @@ def run(version):
             print("неверное значение\nперезапуск")
             error()
         except ValueError:
-            print("что ты несешь\nперезапуск")
+            print("неверное значение\nперезапуск")
             error()
-        except SyntaxError:
-            print("Что-то пошло не так!")
         except FileNotFoundError:
             if preparator("error") == "stopnow":
                 break
